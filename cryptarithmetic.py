beca14eeec70714032d846daa69e37bc33998b72
Raffi Zulvian <raffi16zulvian01muzhaffar@gmail.com>
2021-01-22 23:09:23 +0700

    Complete functionality

--- cryptarithmetic.py
+++ cryptarithmetic.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 
 class Solver:
@@ -12,7 +13,10 @@ class Solver:
 
     def _initMapping(self):
         i = 0
-        allWords = self.answer + self.operand
+        if len(self.operand[0]) == len(self.answer[0]):
+            allWords = self.operand + self.answer
+        else:
+            allWords = self.answer + self.operand
         for word in allWords:
             for letter in word:
                 if letter not in self.mapping.keys():
@@ -39,30 +43,42 @@ class Solver:
         return (self.numOpr, self.numAns)
 
     def _evaluate(self):
+        isOk = True
+        for val in list(self.mapping.values()):
+            if list(self.mapping.values()).count(val) > 1:
+                isOk = False
+                break
         guess = sum(self.numOpr)
-        return guess == self.numAns
+        return (guess == self.numAns) and isOk
+
+    def _checkIsContain(self, val, mapping):
+        if val in mapping.values():
+            return True
+        return False
 
     def _incr(self):
         i = -1
         key = list(self.mapping.keys())
+        val = range(10)
         isFinish = False
         while not isFinish and i >= -1 * len(key):
-            if self.mapping[key[i]] < 9:
-                self.mapping[key[i]] += 1
-                isFinish = True
-            else:
-                self.mapping[key[i]] = 0
+            currVal = self.mapping[key[i]]
+            nextPos = val.index(currVal) + 1
+            if nextPos == 10:
+                nextPos = 0
+                self.mapping[key[i]] = nextPos
                 i -= 1
+            else:
+                while self._checkIsContain(nextPos, self.mapping) and nextPos < 9:
+                    nextPos += 1
+                self.mapping[key[i]] = nextPos
+                isFinish = True
 
     def calculate(self):
         isFound = False
         self._initMapping()
-        print(self.mapping)
         while not isFound:
             guess = self._subtitute()
-            print(self.mapping)
-            print(self.numOpr)
-            print(self.numAns)
             if not guess:
                 self._incr()
                 self.triesCount += 1
@@ -75,15 +91,24 @@ class Solver:
                 self._incr()
         return self.mapping
 
+    def showSolution(self):
+        opr = self.numOpr
+        ans = self.numAns
+        for i in range(len(opr)):
+            space = len(str(ans)) - len(str(opr[i]))
+            if len(opr) - 1 - i == 0:
+                print(' '*space, opr[i], '+', sep='')
+            else:
+                print(' '*space, opr[i], ' '*10, sep='')
+        print('-' * len(str(ans)))
+        print(ans)
 
-def read_file(path):
+
+def readFile(path):
     operand = []
     answer = []
     with open(path) as f:
-        lines = f.read().splitlines()
-        print('PROBLEM', '=======', sep='\n')
-        for line in lines:
-            print(line)
+        lines = [line.strip() for line in f.read().splitlines()]
         lines.pop(-2)
         answer.append(lines.pop(-1))
         lines[-1] = lines[-1].strip('+')
@@ -92,15 +117,32 @@ def read_file(path):
     return (operand, answer)
 
 
-operand, answer = read_file("cryptarithmetic_spec.txt")
+def showProblem(opr, ans):
+    print('\nPROBLEM', '=======', sep='\n')
+    for i in range(len(opr)):
+        space = len(ans[0]) - len(opr[i])
+        if len(opr) - 1 - i == 0:
+            print(' '*space, opr[i], '+', sep='')
+        else:
+            print(' '*space, opr[i], sep='')
+    print('-' * len(ans[0]))
+    print(ans[0])
+
+
+operand, answer = readFile("cryptarithmetic_spec.txt")
+showProblem(operand, answer)
+
 solver = Solver(operand, answer)
 
 print('\nSOLUTION', '========', sep='\n')
 initialTime = time.time()
+print("Calculating...", end='\r')
 solver.calculate()
+sys.stdout.flush()
+solver.showSolution()
 finalTime = time.time()
 
 print("\nEXECUTION TIME: ", end='')
-print(finalTime - initialTime)
+print(finalTime - initialTime, end='')
 print("\nTOTAL TRIES: ", end='')
 print(solver.triesCount)
